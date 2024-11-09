78a9ee6885f427c5714111f88f72d45ec9b375a5
Oleksandr Chernysh <chernysh.olexandr@gmail.com>
2024-11-09 18:23:54 +0200

    Order data is recieved back to server

--- index.js
+++ index.js
@@ -1,169 +1,151 @@
-const TelegramBot = require("node-telegram-bot-api");
-const token = "7808856823:AAEn4sPXRfcXISlK8CQNFhQ5uexDVRIU5ww";
-const bot = new TelegramBot(token, { polling: true });
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { Telegraf } = require('telegraf');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+require('dotenv').config();
 
-// Store user data
-const users = {};
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const webAppUrl = process.env.TELEGRAM_APP_URL; // Replace with your actual ngrok URL
+const bot = new Telegraf(token);
 
-// Keyboard layouts
-const mainKeyboard = {
-  reply_markup: {
-    keyboard: [["Обрати товар"]],
-    resize_keyboard: true,
-    one_time_keyboard: true,
-  },
-};
+const app = express();
+const port = 3001;
 
-const confirmationKeyboard = {
-  reply_markup: {
-    keyboard: [["ТАК", "НІ, ХОЧУ ВИБРАТИ ЩЕ"]],
-    resize_keyboard: true,
-    one_time_keyboard: true,
-  },
-};
+app.use(bodyParser.json());
+app.use(express.json());
+// Add this line to allow all origins, or specify your WebApp's origin
+app.use(cors({ origin: webAppUrl }));
 
-const paymentKeyboard = {
-  reply_markup: {
-    keyboard: [["НАЛОЖКА", "НА РАХУНОК"]],
-    resize_keyboard: true,
-    one_time_keyboard: true,
-  },
-};
+// Local file path for orders CSV
+const path = './orders.csv';
 
-const laterPaymentKeyboard = {
-  reply_markup: {
-    keyboard: [["Я ОПЛАЧУ ПІЗНІШЕ"]],
-    resize_keyboard: true,
-    one_time_keyboard: true,
-  },
-};
-
-// Start command
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  users[chatId] = { state: "start", СПИСОК: "TEST" };
-  bot.sendMessage(chatId, "Здравствуйте, вас вітає крамниця!", mainKeyboard);
+// Check for write access to the orders CSV file
+fs.access(path, fs.constants.W_OK, (err) => {
+    console.log(err ? 'No write access' : 'Write access granted');
 });
 
-// Main logic
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
-
-  if (!users[chatId]) {
-    users[chatId] = { state: "start", СПИСОК: "TEST" };
-  }
-
-  switch (users[chatId].state) {
-    case "start":
-      if (text === "Обрати товар") {
-        users[chatId].state = "select_product";
-        bot.sendMessage(
-          chatId,
-          `Дякуємо за вибір! ${users[chatId].СПИСОК}\nВи підтверджуєте свій вибір?`,
-          confirmationKeyboard
-        );
-      }
-      break;
-
-    case "select_product":
-      if (text === "ТАК") {
-        users[chatId].state = "enter_address";
-        bot.sendMessage(chatId, "Напишіть вашу адресу для Нової Пошти");
-      } else if (text === "НІ, ХОЧУ ВИБРАТИ ЩЕ") {
-        bot.sendMessage(
-          chatId,
-          `Дякуємо за вибір! ${users[chatId].СПИСОК}\nВи підтверджуєте свій вибір?`,
-          confirmationKeyboard
-        );
-      }
-      break;
-
-    case "enter_address":
-      users[chatId].АДРЕСА = text;
-      users[chatId].state = "confirm_address";
-      bot.sendMessage(
-        chatId,
-        `Ваша адреса така: ${text}, підтверджуєте?`,
-        confirmationKeyboard
-      );
-      break;
-
-    case "confirm_address":
-      if (text === "ТАК") {
-        users[chatId].state = "choose_payment";
-        bot.sendMessage(
-          chatId,
-          "Ви хочете оплатити наложкою чи на рахунок?",
-          paymentKeyboard
-        );
-      } else if (text === "НІ, ХОЧУ ВИБРАТИ ЩЕ") {
-        users[chatId].state = "enter_address";
-        bot.sendMessage(chatId, "Напишіть вашу адресу для Нової Пошти");
-      }
-      break;
-
-    case "choose_payment":
-      if (text === "НАЛОЖКА") {
-        users[chatId].ОПЛАТА = "Наложка";
-        users[chatId].state = "enter_comment";
-        bot.sendMessage(
-          chatId,
-          "Якщо ви маєте якісь коментарі та побажання - саме час ними поділитися:"
-        );
-      } else if (text === "НА РАХУНОК") {
-        users[chatId].state = "wait_payment";
-        bot.sendMessage(
-          chatId,
-          "Будь ласка, оплатіть на рахунок UA1234567890, та скиньте скріншот оплати"
-        );
-      }
-      break;
-
-    case "wait_payment":
-      if (msg.photo) {
-        users[chatId].ОПЛАТА = "Оплату виконано";
-        users[chatId].state = "enter_comment";
-        bot.sendMessage(
-          chatId,
-          "Якщо ви маєте якісь коментарі та побажання - саме час ними поділитися:"
-        );
-      } else if (text === "Я ОПЛАЧУ ПІЗНІШЕ") {
-        users[chatId].ОПЛАТА = "Очікується оплата";
-        users[chatId].state = "enter_comment";
-        bot.sendMessage(
-          chatId,
-          "Якщо ви маєте якісь коментарі та побажання - саме час ними поділитися:"
-        );
-      } else {
-        bot.sendMessage(
-          chatId,
-          "Будь ласка, оплатіть на рахунок UA1234567890, та скиньте скріншот оплати",
-          laterPaymentKeyboard
-        );
-      }
-      break;
-
-    case "enter_comment":
-      users[chatId].КОМЕНТАР = text;
-      users[chatId].state = "finish";
-      let finalMessage = `Нарешті, все!\nДякуємо за замовлення!\nВи замовили: ${users[chatId].СПИСОК}\nВаша адреса: ${users[chatId].АДРЕСА}\nВаш коментар: ${users[chatId].КОМЕНТАР}\nСтатус оплати: ${users[chatId].ОПЛАТА}\n`;
-
-      if (users[chatId].ОПЛАТА === "Очікується оплата") {
-        finalMessage +=
-          "Надішліть скриншот оплати в цього бота та ваше замовлення буде відправлено найближчим часом";
-      } else {
-        finalMessage += "Ваше замовлення буде відправлено найближчим часом!";
-      }
-
-      bot.sendMessage(chatId, finalMessage);
-      delete users[chatId]; // Reset user state
-      break;
-
-    default:
-      bot.sendMessage(
-        chatId,
-        "Щось пішло не так. Будь ласка, почніть спочатку з команди /start"
-      );
-  }
+// CSV writer for orders
+const orderWriter = createCsvWriter({
+    path: 'orders.csv', // Path to the local CSV file
+    header: [
+        { id: 'orderId', title: 'OrderID' },
+        { id: 'receiver', title: 'Receiver' },
+        { id: 'phone', title: 'Phone' },
+        { id: 'city', title: 'City' },
+        { id: 'region', title: 'Region' },
+        { id: 'address', title: 'Address' },
+        { id: 'paymentMethod', title: 'PaymentMethod' },
+        { id: 'total', title: 'Total' },
+        { id: 'products', title: 'Products' },
+    ],
 });
+
+// Start command for the bot
+bot.start((ctx) => {
+    ctx.reply('Вітаю! Ви можете обрати товари в нашому міні-додатку', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Відкрити міні-додаток', web_app: { url: webAppUrl } }],
+            ],
+        },
+    });
+});
+
+// Handle incoming order data from the mini app
+bot.on('message', (ctx) => {
+    if (ctx.message.web_app_data) {
+        const chatId = ctx.message.chat.id;
+
+        try {
+            const orderDetails = JSON.parse(ctx.message.web_app_data.data);
+
+            // Check if required properties exist
+            if (!orderDetails.delivery || !orderDetails.products || !orderDetails.finalTotal || !orderDetails.paymentMethod) {
+                ctx.reply('Сталася помилка: дані замовлення неповні.');
+                return;
+            }
+
+            // Save the order to the local CSV file
+            orderWriter
+                .writeRecords([
+                    {
+                        orderId: `order${Date.now()}`,
+                        receiver: orderDetails.delivery.receiver,
+                        phone: orderDetails.delivery.phone,
+                        city: orderDetails.delivery.city,
+                        region: orderDetails.delivery.region,
+                        address: orderDetails.delivery.np,
+                        paymentMethod: orderDetails.paymentMethod,
+                        total: orderDetails.finalTotal,
+                        products: orderDetails.products.map((p) => `${p.name} x ${p.quantity}`).join(', '),
+                    },
+                ])
+                .then(() => {
+                    let message = `Дякуємо за замовлення!\n\nВи замовили:\n`;
+                    orderDetails.products.forEach((product) => {
+                        message += `- ${product.name} x ${product.quantity}\n`;
+                    });
+                    message += `\nЗагальна сума зі знижкою: ${orderDetails.finalTotal.toFixed(2)} грн`;
+
+                    message += orderDetails.paymentMethod === 'peredplata'
+                        ? `\n\nРахунок для оплати: 1112223333444545`
+                        : `\n\nОплата при отриманні`;
+
+                    ctx.reply(message);
+                })
+                .catch((error) => {
+                    console.error('Error saving order to CSV:', error);
+                    ctx.reply('Сталася помилка під час оформлення замовлення. Спробуйте пізніше.');
+                });
+        } catch (error) {
+            console.error('Error processing web_app_data:', error);
+            ctx.reply('Сталася помилка з отриманими даними. Спробуйте ще раз.');
+        }
+    }
+});
+
+// Handle any text message
+bot.on('text', (ctx) => {
+    console.log('Received a text message:', ctx.message.text);
+});
+
+app.post('/save-order', (req, res) => {
+    // Handle the request and send a response
+    console.log(req.body); // Log the incoming data
+    // Respond with a JSON object
+    res.status(200).json({
+        message: 'Order saved successfully',
+        orderId: 12345  // Example additional data
+    });
+});
+
+// Webhook route to handle Telegram updates
+app.post('/webhook', (req, res) => {
+    bot.handleUpdate(req.body, res);
+    res.sendStatus(200); // Send a 200 OK response
+});
+
+// Initialize the bot webhook
+const initBotWebhook = async () => {
+    const webhookUrl = `${webAppUrl}/webhook`; // Replace with your actual ngrok URL
+
+    try {
+        await bot.telegram.setWebhook(webhookUrl);
+        console.log(`Webhook set to ${webhookUrl}`);
+    } catch (error) {
+        console.error('Error setting webhook:', error);
+    }
+};
+
+// Call the function to set the webhook
+initBotWebhook();
+
+// Start the express server
+app.listen(port, () => {
+    console.log(`Bot server is running on http://localhost:${port}`);
+});
+
+// Launch the bot
+bot.launch();
